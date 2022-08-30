e1a5dc134958fd5d1682bd8b41d773383db85a96
Maxim <maximkut@gmail.com>
2022-08-30 20:50:58 +0300

    Zadacha1

---
+++ Vtornik/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число 1");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = Convert.ToInt16(Console.ReadLine());
+
+if (b==a*a)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
