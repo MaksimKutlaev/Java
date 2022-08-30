a52ff4aa67ca260203c63d5282f6d247a55f342f
Maxim <maximkut@gmail.com>
2022-08-30 21:01:47 +0300

    Zadacha2

---
+++ Vtornik/Zadacha2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Введите целое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+switch (x)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресение");
+        break;
+}
