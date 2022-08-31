0e3a52d5791efe526f8051b76b7aa97ad2e568f4
Maxim <maximkut@gmail.com>
2022-08-31 21:49:38 +0300

    добавил пример нахождения большего числа

---
+++ HomeWork/HM1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ( "Ведите число a: " );
+int a=Convert.Tolnt32(Console.ReadLine());
+Console.Write ( "Ведите число b: " );
+int b=Convert.Tolnt32(Console.ReadLine());
+int max=a;
+
+if (a>b)
+    {
+    Console.WriteLine(a);
+    }
+else
+Console.WriteLine(b);
