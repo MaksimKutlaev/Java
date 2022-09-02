8f10f6743fdee3e1619c459a4a3ecc273c5810e4
Maxim <maximkut@gmail.com>
2022-09-02 22:24:10 +0300

    Add HM3

---
+++ HomeWork/HM3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Ведите число :");
+int x=Convert.ToInt32(Console.ReadLine());
+if (x==0)
+    {   
+    Console.WriteLine("Вы ввели ноль");
+    } 
+else if (x%2 == 0) 
+    {
+    Console.WriteLine("Число является четным");
+    } 
+else 
+    {
+    Console.WriteLine("Число является нечетным");
+    }
