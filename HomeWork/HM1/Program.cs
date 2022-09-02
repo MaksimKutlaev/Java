7a6a49ca07782937d6e34e5f530996790dbe4286
Maxim <maximkut@gmail.com>
2022-09-02 21:57:16 +0300

    Add HM1.1

--- HomeWork/HM1/Program.cs
+++ HomeWork/HM1/Program.cs
@@ -1,8 +1,12 @@
 ﻿Console.Write ( "Ведите число a: " );
-int a=Convert.Tolnt32(Console.ReadLine());
+//int a=Convert.ToInt32(Console.ReadLine());
+string aStr = Console.ReadLine();
+int a = Convert.ToInt32(aStr);
 Console.Write ( "Ведите число b: " );
-int b=Convert.Tolnt32(Console.ReadLine());
-int max=a;
+int b=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(aStr+b);
+//int max=a;
 
 if (a>b)
     {
