9176c513adbd39fd5643e12b21a00eeddccba802
Maxim <maximkut@gmail.com>
2022-09-08 23:03:18 +0300

    added Zadacha13

--- HomeWork/HM13/Program.cs
+++ HomeWork/HM13/Program.cs
@@ -1,4 +1,17 @@
 ﻿
 Console.WriteLine("Введите целое число: ");
 int n=Convert.ToInt32(Console.ReadLine());
-int x=n
+int z=10;
+while (n/z>0)
+{
+    z=z*10;
+}
+    if (z<1000)
+{
+    Console.WriteLine("Третьей цифры нет"); 
+    return;
+}
+
+int d=n%(z/100);
+int result = d/(z/1000);
+Console.WriteLine(result);
