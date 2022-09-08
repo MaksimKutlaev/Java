27025c38a84b911047d0a916f7e96f1368baeece
Maxim <maximkut@gmail.com>
2022-09-08 23:28:01 +0300

    added Zadacha15

---
+++ HomeWork/HM15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int n=Convert.ToInt32(Console.ReadLine());
+if(n<6) 
+    {
+    Console.WriteLine("Не является выходным");
+    }
+else if(n>5 && n<8)
+    {
+    Console.WriteLine("Выходной");
+    }
+else
+    { 
+    Console.Write("Неверное число");
+    }
