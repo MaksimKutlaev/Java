3c5ee2987aa171bca7dc33523266948a7c5db24d
Maxim <maximkut@gmail.com>
2022-09-12 20:07:05 +0300

    added HM23

---
+++ HomeWork/HM23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+string squareTable(int n)
+{
+string result = $"{n} -> 1";
+
+for (int i = 2; i <= n; i++)
+{
+result = result + $", {Math.Pow(i, 2)}";
+
+}
+
+return result;
+}
+
+try
+{
+System.Console.Write("Введите число ");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+System.Console.WriteLine(squareTable(n));
+
+
+}
+catch
+{
+System.Console.WriteLine("Вводите целочисленное значение");
+}
