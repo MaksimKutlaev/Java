d7a1faffeb5325d99cfc28f82531392692c58f60
Maxim <maximkut@gmail.com>
2022-09-13 22:59:54 +0300

    added Z30

--- Vtornik/Z30/Program.cs
+++ Vtornik/Z30/Program.cs
@@ -1,2 +1,21 @@
 ﻿// Напишите программу, которая заполняет и выводит массив из N элементов, 
 // N - задается с клавиатуры, заполненный целыми случайными числами от 1 до 99.
+int[] RndMassive(int N)
+{
+    int[] array=new int[N];
+    for(int i=0;i<N;i++)
+    {
+        array[i]=new Random().Next(100);
+    }
+    return array;
+}
+try
+    {
+    System.Console.Write("Введите размер массива = ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(String.Join(",",RndMassive(a)));
+    }
+catch
+    {
+    System.Console.WriteLine("Введено не число, или число больше, чем может вместить int");
+    }
