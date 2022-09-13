4e9a03624706a6179bfdd8a05f9781f706227b7b
Maxim <maximkut@gmail.com>
2022-09-13 22:37:01 +0300

    added Z28

--- Vtornik/Z26/Program.cs
+++ Vtornik/Z26/Program.cs
@@ -3,43 +3,43 @@
 // 78 -> 2
 // 89126 -> 5
 
-int SchetNum(int n)
-{
-    int n=0;
-    int i=1;
-    while (i>=0)
-    {
-        i=i/10;
-        i++;
-    }
-    return schet;
-}
-try
-{
-    Console.Write("Введите целое число ");
-    int a=Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine($"Кол-во цифр в числе {a} = {SchetNum(a)}" );
-}
-catch
-{
-    Console.WriteLine("Неправильно ввели");
-}
-
-// int Digit (int N)
+// int SchetNum(int n)
 // {
-
-// int i = 0;
-// while (N/((int)Math.Pow((Double)10,i))!=0) i++;
-// return i;
+//     int n=0;
+//     int i=1;
+//     while (i>=0)
+//     {
+//         i=i/10;
+//         i++;
+//     }
+//     return schet;
 // }
 // try
 // {
-// Console.Write ("Введите целое число ");
-// int a = Convert.ToInt32 (Console.ReadLine());
-// Console.WriteLine($"Количество цифр в числе {a} равно {Digit(a)}");
+//     Console.Write("Введите целое число ");
+//     int a=Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine($"Кол-во цифр в числе {a} = {SchetNum(a)}" );
 // }
-// catch (System.Exception)
+// catch
 // {
-
-// Console.WriteLine("Надо было вводить именно целое число!");
+//     Console.WriteLine("Неправильно ввели");
 // }
+
+int Digit (int N)
+{
+
+int i = 0;
+while (N/((int)Math.Pow((Double)10,i))!=0) i++;
+return i;
+}
+try
+{
+Console.Write ("Введите целое число ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе {a} равно {Digit(a)}");
+}
+catch (System.Exception)
+{
+
+Console.WriteLine("Надо было вводить именно целое число!");
+}
---
+++ Vtornik/Z28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+//5 -> 120
+
+int factorial(int N)
+{
+int result = 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        result = result * i;
+
+    }
+    return result;
+}
+try
+    {
+    System.Console.Write("Введите число = ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(factorial(a));
+    }
+catch
+    {
+    System.Console.WriteLine("Введено не число, или число больше, чем может вместить int");
+    }
