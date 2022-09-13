34c920ddf313823ecffd97469defd138f31477a7
Maxim <maximkut@gmail.com>
2022-09-13 23:05:42 +0300

    added Z30.1

--- Vtornik/Try/Program.cs
+++ Vtornik/Try/Program.cs
@@ -1,25 +1,23 @@
 ﻿Console.Write ("Введите целое число ");
-            try
-                {
-                int x=Convert.ToInt32 (Console.ReadLine());
-            //int y = x*x;
-                Console.WriteLine("Квадрат этого числа равен "+x*x);
-                if (x>0)
-                {
-                    Console.WriteLine("Число положительное");
-                }
-                else if (x<0)  Console.WriteLine("Число отрицательное");
-                else Console.WriteLine("Вы ввели ноль"); 
-                }
-                //выведу все числа от 0 до x
-                int i = 0;
-                while (i<=x) 
-                    {
-                        Console.Write (i+", "); 
-                        i++; 
-                    }
-            catch 
-            {
-                
-                Console.WriteLine("надо было вводить именно целое число!");
-            }
+try
+    {
+        int x=Convert.ToInt32 (Console.ReadLine());
+        Console.WriteLine("Квадрат этого числа равен "+x*x);
+        if (x>0)
+        {
+            Console.WriteLine("Число положительное");
+        }
+        else if (x<0)  Console.WriteLine("Число отрицательное");
+        else Console.WriteLine("Вы ввели ноль"); 
+    }
+        //выведу все числа от 0 до x
+    int i = 0;
+        while (i<=x) 
+        {
+            Console.Write (i+", "); 
+            i++; 
+        }
+catch 
+    {
+    Console.WriteLine("надо было вводить именно целое число!");
+    }   
