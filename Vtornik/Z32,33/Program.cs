4044a0bc92f145cb5ff31dafaa6f1ff7897656be
Maxim <maximkut@gmail.com>
2022-09-16 22:19:13 +0300

    added 32-33

--- Vtornik/Z32,33/Program.cs
+++ Vtornik/Z32,33/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
-/* 
+
 int[] FillArray(int size)
 {
     int[] mas = new int[size];
@@ -30,10 +30,9 @@ void PrintArray(int[] mmm)
             Console.WriteLine($"{mmm[i]}]");
     }
 }
-int[] array = FillArray(4);
-PrintArray(array);
-PrintArray(ChangeZ(array));
-*/
+// int[] array = FillArray(4);
+// PrintArray(array);
+// PrintArray(ChangeZ(array));
 // Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, 3] -> да
