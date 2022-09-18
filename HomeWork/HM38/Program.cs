577e41d549d738dab6aa36ee5e0ce1f661c512ba
Maxim <maximkut@gmail.com>
2022-09-18 19:46:19 +0300

    add HM38

--- HomeWork/HM38/Program.cs
+++ HomeWork/HM38/Program.cs
@@ -1,32 +1,32 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
-int[] FillArray(int size)
+double[] FillArray(int size)
 {
     double[] mas = new double[size];
 
-    for (double i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        mas[i] = new Random().Next(-20,49);
+        mas[i] = new Random().NextDouble()*100;
     }
     return mas;
 }
 void PrintArray(double[] mmm)
 {
     Console.Write("[");
-    for (double i = 0; i < mmm.Length; i++)
+    for (int i = 0; i < mmm.Length; i++)
     {
         if (i != mmm.Length - 1)
-            Console.Write($"{mmm[i]}, ");
+            Console.Write($"{mmm[i]:F2}, ");
         else
-            Console.WriteLine($"{mmm[i]}]");
+            Console.WriteLine($"{mmm[i]:F2}]");
     }
 }
-double Minmax(double[] array)
+double DiffMinmax(double[] array)
   {
     
     double max=array[0];
     double min=array[0];
-    for(double i=0;i<array.Length;i++)
+    for(int i=0;i<array.Length;i++)
     {
       if (array[i]>max)
        max=array[i];
@@ -37,14 +37,8 @@ double Minmax(double[] array)
     return diff;
     
           
-  }
-//   double Difference (double max, double min);
-// double diff=max-min;
-//         return diff;
-
-
+}
 double[] array = FillArray(4);
 PrintArray(array);
-double diff = Minmax(array);
-Console.WriteLine($"Разница между max и min = {diff}");
-
+double diff = DiffMinmax(array);
+Console.WriteLine($"Разница между max и min = {diff:F2}");
