6bc7de6a95854a71358d53fb42a7c1c4618a2e3a
Maxim <maximkut@gmail.com>
2022-09-23 20:31:04 +0300

    add HM43

--- HomeWork/HM43/Program.cs
+++ HomeWork/HM43/Program.cs
@@ -2,28 +2,20 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.Write("Введите k1: ");
-int b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-int k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-int b2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-int k2 = Convert.ToDouble(Console.ReadLine());
- 
- 
+void Cross(double b1, double k1, double b2, double k2)
+{
 double x = -(b1 - b2) / (k1 - k2);
 double y = k1 * x + b1;
- 
-// x = Math.Round(x, 3);
-// y = Math.Round(y, 3);
- 
-// Console.Write("Введите k1: ");
-// int b1 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Введите b1: ");
-// int k1 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Введите k2: ");
-// int b2 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Введите b2: ");
-// int k2 = Convert.TToDouble(Console.ReadLine());
 Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
+
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Cross(b1, k1, b2, k2);
