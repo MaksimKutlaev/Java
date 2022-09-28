15fc2c1e97a8ca68b6afc2fa547e29a1fe9aa9ba
Maxim <maximkut@gmail.com>
2022-09-28 15:36:10 +0300

    add Z55

--- Vtornik/Z53/Program.cs
+++ Vtornik/Z53/Program.cs
@@ -36,6 +36,7 @@ int[,] ChangeRow(int[,] mas)
         temp=mas[0,i];
         mas[0,i]=mas[mas.GetLength(0)-1,i];
         mas[mas.GetLength(0)-1,i]=temp;
+        //(arr[0,j],arr[arr.GetLength(0)-1,j]) = (arr[arr.GetLength(0)-1,j],arr[0,j]);
     }
     return mas;
 }
@@ -93,6 +94,19 @@ int[,] ChangeRowColum(int[,] arr)
     }
     return mas;
 }
+// int [,] ChangeColRow (int [,] arr)
+// {
+// if (arr.GetLength (0) != arr.GetLength (1)) Console.WriteLine ("cannot change rows and columns");
+// else
+// {
+// for (int i = 0; i < arr.GetLength (0); i++)
+// {
+// for (int j = i+1; j < arr.GetLength (1); j++) (arr [i, j] , arr [j, i]) = (arr [j, i] , arr [i, j]);
+
+// }
+// }
+// return arr;
+// }
 
 System.Console.Write("Введите количество строк n = ");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -103,3 +117,39 @@ PrintArray(newArray);
 int[,] array = ChangeRowColum(newArray);
 Console.WriteLine();
 PrintArray(array);
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] Square(int m)
+{
+    int[,] mas = new int[m, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            mas[i, j] = new Random().Next(10);
+        }
+    }
+
+    return mas;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j != arr.GetLength(1) - 1)
+                Console.Write($"{arr[i, j]}, ");
+            else if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1)
+                Console.WriteLine($"{arr[i, j]}");
+            else if (j == arr.GetLength(1) - 1)
+                Console.WriteLine($"{arr[i, j]},");
+        }
+    }
+}
