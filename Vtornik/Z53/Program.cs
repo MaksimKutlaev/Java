603713c0d4de3c6a6cb0027e667d43b22feaca4e
Maxim <maximkut@gmail.com>
2022-09-28 15:11:58 +0300

    add Z55

--- Vtornik/Z53/Program.cs
+++ Vtornik/Z53/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
-// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, 
-// программа должна вывести сообщение для пользователя.
-
+/*
 int[,] FillMas(int n, int m)
 {
     int[,] mas = new int[n, m];
@@ -49,5 +47,59 @@ int m = Convert.ToInt32(Console.ReadLine());
 int[,] newArray = FillMas(n, m);
 PrintArray(newArray);
 int[,] array = ChangeRow(newArray);
+Console.WriteLine();
+PrintArray(array);
+*/
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+int[,] FillMas(int n, int m)
+{
+    int[,] mas = new int[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            mas[i, j] = new Random().Next(10);
+        }
+    }
+
+    return mas;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j != arr.GetLength(1) - 1)
+                Console.Write($"{arr[i, j]}, ");
+            else if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1)
+                Console.WriteLine($"{arr[i, j]}");
+            else if (j == arr.GetLength(1) - 1)
+                Console.WriteLine($"{arr[i, j]},");
+        }
+    }
+}
+int[,] ChangeRowColum(int[,] arr)
+{
+    int[,] mas = new int[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            mas[i, j] = arr[j, i];
+        }
+    }
+    return mas;
+}
+
+System.Console.Write("Введите количество строк n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] newArray = FillMas(n, m);
+PrintArray(newArray);
+int[,] array = ChangeRowColum(newArray);
 Console.WriteLine();
 PrintArray(array);
