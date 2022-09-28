4cf2617129a47bb96af57964fe1988e8f53acfc4
Maxim <maximkut@gmail.com>
2022-09-28 14:55:21 +0300

    add Z53

---
+++ Vtornik/Z53/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+
+int[,] FillMas(int n, int m)
+{
+    int[,] mas = new int[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            mas[i, j] = new Random().Next(10);
+        }
+    }
+
+    return mas;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j != arr.GetLength(1) - 1)
+                Console.Write($"{arr[i, j]}, ");
+            else if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1)
+                Console.WriteLine($"{arr[i, j]}");
+            else if (j == arr.GetLength(1) - 1)
+                Console.WriteLine($"{arr[i, j]},");
+        }
+    }
+}
+int[,] ChangeRow(int[,] mas)
+{
+    int temp=0;
+    for (int i = 0; i < mas.GetLength(1); i++)
+    {
+        temp=mas[0,i];
+        mas[0,i]=mas[mas.GetLength(0)-1,i];
+        mas[mas.GetLength(0)-1,i]=temp;
+    }
+    return mas;
+}
+
+System.Console.Write("Введите количество строк n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] newArray = FillMas(n, m);
+PrintArray(newArray);
+int[,] array = ChangeRow(newArray);
+Console.WriteLine();
+PrintArray(array);
